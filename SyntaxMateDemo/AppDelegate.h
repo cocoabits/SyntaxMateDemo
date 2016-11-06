8645a4e528c9ad9856984376d4bb8d0f413abb27
Vadim Shpakovski <vadim@shpakovski.com>
2016-11-06 20:49:55 +0300

    Initial commit with Demo project

---
+++ SyntaxMateDemo/AppDelegate.h
@@ -0,0 +1,4 @@
+#import <Foundation/Foundation.h>
+
+@interface AppDelegate : NSObject
+@end
---
+++ SyntaxMateDemo/AppDelegate.m
@@ -0,0 +1,16 @@
+#import "AppDelegate.h"
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate () <NSApplicationDelegate>
+
+@property (nonatomic, weak) IBOutlet NSWindow *window;
+
+@end
+
+@implementation AppDelegate
+
+- (void)applicationWillFinishLaunching:(NSNotification *)aNotification {
+}
+
+@end
